b105d90acebe6c98ba5583f75d42302a45548ef2
Martín Rosselot <martin.rosselot@uc.cl>
2024-07-05 12:29:38 -0400

    Limpieza y ejecucion

---
+++ Visualizacion/src/EscalasSeparadas.js
@@ -0,0 +1,172 @@
+const width_map = 1800
+const height_map = 800
+
+const color_vacantes = "red"
+const color_postulaciones = "yellow"
+
+radius_max = 3
+
+d3.select("#Title").text("Postulaciones y Vacantes tienen escalas separadas")
+
+
+d3.select("#vacantes").style("background-color", color_vacantes)
+d3.select("#postulaciones").style("background-color", color_postulaciones)
+
+const map = d3.select("#map").append("svg")
+    .attr("width", width_map)
+    .attr("height", height_map)
+    .style("background-color", "#f0f0f0")
+    .style("border", "1px solid black")
+    .style("border-radius", "10px");
+
+const paths = map.append("g")
+    .attr("id", "paths")
+const hexagonos = map.append("g")
+    .attr("id", "hexagonos")
+
+
+// Define zoom behavior
+const zoom = d3.zoom()
+    .scaleExtent([0.8, 50]) // Limita el nivel de zoom (mínimo 1x y máximo 10x)
+    .on("zoom", (event) => {
+        paths.attr("transform", event.transform);
+        hexagonos.attr("transform", event.transform);
+    });
+
+// Apply zoom behavior to the SVG
+map.call(zoom);
+
+
+d3.json("../data/comunas_ps.geojson").then(joinComunas)
+d3.csv("../data/informacion_establecimientos.csv", d3.autoType).then(joinEstablecimientos)
+
+
+
+function joinComunas(datos) {
+    const menor_densidad = d3.min(datos.features, (d) => d.properties.n_postulaciones / d.properties.vacantes);
+    const mayor_densidad = d3.max(datos.features, (d) => d.properties.n_postulaciones / d.properties.vacantes);
+
+    const escalaColoracionDensidad = d3
+        .scaleSequential()
+        .interpolator(d3.interpolate('white', 'steelblue'))
+        .domain([menor_densidad, mayor_densidad]);
+    
+    const enter = (enter) => {
+
+        const g = enter.append("g")
+            .attr("class", "comuna")
+
+        g.append("path")
+            .attr("d", caminosGeo)
+            .attr("fill", "white")
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.1)
+            .attr("fill", (d) => escalaColoracionDensidad(d.properties.n_postulaciones / d.properties.vacantes))
+            .attr("z-index", 1)
+
+        g.append("text")
+            .text(d => Math.round((d.properties.n_postulaciones / d.properties.vacantes)*100)/100)
+            .attr("x", d => caminosGeo.centroid(d)[0])
+            .attr("y", d => caminosGeo.centroid(d)[1])
+            .attr("text-anchor", "middle")
+            .attr("dy", 5)
+            .attr("fill", "white")
+            .attr("font-size", 10)
+            .attr("fill", (d) =>((d.properties.n_postulaciones / d.properties.vacantes) < menor_densidad + 0.52*(mayor_densidad-menor_densidad)) ? "black" : "white")
+            .attr("z-index", 1)
+    }
+
+    const update = (update) => {
+    }
+
+    const exit = (exit) => {
+    }
+
+    const proyeccion = d3.geoMercator()
+        .fitSize([width_map, height_map-20], datos);
+    
+    const caminosGeo = d3.geoPath().projection(proyeccion);
+
+    paths.selectAll(".comuna")
+        .data(datos.features, d => d.properties.COMUNA)
+        .join(enter, update, exit);       
+    
+        
+    paths.selectAll(".comuna")
+        .on("click", (evento, d) => {
+        console.log(d.properties.COMUNA)
+        console.log(d.properties)
+    })
+}
+
+function joinEstablecimientos(datos) {
+    d3.json("../data/comunas_ps.geojson").then(geo => {
+
+        const proyeccion = d3.geoMercator()
+            .fitSize([width_map, height_map-20], geo);
+
+        const hexbin = d3.hexbin()
+            .extent([[0, 0], [width_map, height_map-20]])
+            .radius(radius_max)
+            .x(d => proyeccion([d.LONGITUD, d.LATITUD])[0])
+            .y(d => proyeccion([d.LONGITUD, d.LATITUD])[1])
+        
+        const bins = hexbin(datos);
+
+        const menor_cantidad_vacantes = d3.min(bins, (d) => d3.sum(d, (e) => e.vacantes));
+        const mayor_cantidad_vacantes = d3.max(bins, (d) => d3.sum(d, (e) => e.vacantes));
+
+        console.log(menor_cantidad_vacantes, mayor_cantidad_vacantes)
+        const escalaVacantes = d3
+            .scaleLinear()
+            .domain([menor_cantidad_vacantes, mayor_cantidad_vacantes])
+            .range([0, 1]);
+
+        
+        const menor_cantidad_postulaciones = d3.min(bins, (d) => d3.sum(d, (e) => e.n_postulaciones));
+        const mayor_cantidad_postulaciones = d3.max(bins, (d) => d3.sum(d, (e) => e.n_postulaciones));
+
+        const escalaPostulaciones = d3
+            .scaleLinear()
+            .domain([menor_cantidad_postulaciones, mayor_cantidad_postulaciones])
+            .range([0, 1]);
+        
+        
+
+        const enter = (enter) => {
+
+            enter.append("path")
+                .attr("d", hexbin.hexagon())
+                .attr("transform", d => `translate(${d.x}, ${d.y}), scale(${escalaVacantes(d3.sum(d, (e) => e.vacantes))})`)
+                .attr("fill", color_vacantes)
+                .attr("z-index", 2)
+                .attr("opacity", 0.5)
+
+            enter.append("path")
+                .attr("d", hexbin.hexagon())
+                .attr("transform", d => `translate(${d.x}, ${d.y}), scale(${escalaPostulaciones(d3.sum(d, (e) => e.n_postulaciones))})`)
+                .attr("fill", color_postulaciones)
+                .attr("z-index", 1)
+                .attr("opacity", 0.5)
+            
+            
+            
+            enter.append("path")
+                .attr("d", hexbin.hexagon())
+                .attr("transform", d => `translate(${d.x}, ${d.y})`)
+                .attr("stroke", "black")
+                .attr("fill", "none")
+                .attr("stroke-width", 0.01)
+        }           
+
+        const update = (update) => {
+        }
+
+        const exit = (exit) => {
+        }
+        
+        hexagonos.selectAll("path")
+            .data(bins, d => d.x + ":" + d.y)
+            .join(enter, update, exit);        
+    })
+}
---
+++ Visualizacion/utils/comunas_ps.py
@@ -0,0 +1,125 @@
+import json
+import pandas as pd
+
+print("Leyendo datos geojson...")
+with open('../data/comunas_ps.geojson', encoding='utf-8') as f:
+  datos = json.load(f)
+  print("Datos Leídos")
+
+postulaciones_por_establecimiento = {}
+postulaciones_totales = 0
+
+print("Leyendo archivo csv...")
+archivo = pd.read_csv('../data/postulaciones.csv', encoding='utf-8')
+print("Archivo leído")
+
+print("Contando postulaciones por establecimiento...")
+for fila in archivo.iterrows():
+    establecimiento = fila[1]['rbd']
+    if establecimiento not in postulaciones_por_establecimiento:
+        postulaciones_por_establecimiento[establecimiento] = 0
+    postulaciones_por_establecimiento[establecimiento] += 1
+    postulaciones_totales += 1
+print("Conteo finalizado")
+
+oferta_por_establecimientos = {}
+vacantes_totales = 0
+
+print("Leyendo archivo csv...")
+archivo = pd.read_csv('../data/Oferta.csv', encoding='utf-8', sep=';')
+archivo = archivo[archivo['rbd'].isin(postulaciones_por_establecimiento.keys())]
+print("Archivo leído")
+
+print("Contando oferta por establecimiento...")
+for fila in archivo.iterrows():
+    establecimiento = fila[1]['rbd']
+    if establecimiento not in oferta_por_establecimientos:
+        oferta_por_establecimientos[establecimiento] = 0
+    oferta_por_establecimientos[establecimiento] += fila[1]['vacantes']
+    vacantes_totales += fila[1]['vacantes']
+print("Conteo finalizado")
+
+suma_proporciones_vacantes = 0
+suma_proporciones_postulaciones = 0
+print("Agregando información por establecimiento...")
+establecimientos = pd.read_csv('../data/informacion_establecimientos.csv', encoding='utf-8')  
+for row in establecimientos.iterrows():
+    rbd = row[1]['RBD']
+    if rbd in postulaciones_por_establecimiento:
+        establecimientos.loc[row[0], 'n_postulaciones'] = postulaciones_por_establecimiento[rbd]
+        establecimientos.loc[row[0], 'n_postulaciones_p'] = postulaciones_por_establecimiento[rbd]/postulaciones_totales
+        suma_proporciones_postulaciones += postulaciones_por_establecimiento[rbd]/postulaciones_totales
+    if rbd in oferta_por_establecimientos:
+        establecimientos.loc[row[0], 'vacantes'] = oferta_por_establecimientos[rbd]
+        establecimientos.loc[row[0], 'vacantes_p'] = oferta_por_establecimientos[rbd]/vacantes_totales
+        suma_proporciones_vacantes += oferta_por_establecimientos[rbd]/vacantes_totales
+  
+establecimientos.to_csv('../data/informacion_establecimientos.csv', index=False)
+print("Información agregada")
+print("Suma de proporciones de vacantes: ", suma_proporciones_vacantes)
+print("Suma de proporciones de postulaciones: ", suma_proporciones_postulaciones)
+
+
+vacantes_por_comuna = {}
+
+print("Leyendo archivo csv...")
+archivo = pd.read_csv('../data/informacion_establecimientos.csv', encoding='utf-8')
+print("Archivo leído")
+
+print("Contando postulaciones por comuna...")
+for fila in archivo.iterrows():
+    comuna = fila[1]['NOM_COM_RBD']
+    if comuna not in vacantes_por_comuna:
+        vacantes_por_comuna[comuna] = 0
+    vacantes_por_comuna[comuna] += fila[1]['vacantes']
+print("Conteo finalizado")
+   
+establecimientos_por_comuna = {}
+
+print("Leyendo archivo csv...")
+archivo = pd.read_csv('../data/informacion_establecimientos.csv', encoding='utf-8')
+print("Archivo leído")
+
+print("Contando establecimientos por comuna...")
+for fila in archivo.iterrows():
+    comuna = fila[1]['NOM_COM_RBD']
+    if comuna not in establecimientos_por_comuna:
+        establecimientos_por_comuna[comuna] = 0
+    establecimientos_por_comuna[comuna] += 1
+print("Conteo finalizado")
+
+postulaciones_por_comuna = {}
+
+print("Leyendo archivo csv...")
+archivo = pd.read_csv('../data/informacion_establecimientos.csv', encoding='utf-8')
+print("Archivo leído")
+
+print("Contando postulaciones por comuna...")
+for fila in archivo.iterrows():
+    comuna = fila[1]['NOM_COM_RBD']
+    if comuna not in postulaciones_por_comuna:
+        postulaciones_por_comuna[comuna] = 0
+    postulaciones_por_comuna[comuna] += fila[1]['n_postulaciones']
+print("Conteo finalizado")
+
+print("Agregando información por comuna...")
+for comuna in datos['features']:
+    nombre_comuna = comuna['properties']['COMUNA']
+  
+    if nombre_comuna in establecimientos_por_comuna:
+        comuna['properties']['n_establecimientos'] = establecimientos_por_comuna[nombre_comuna]
+  
+    if nombre_comuna in postulaciones_por_comuna:
+        comuna['properties']['n_postulaciones'] = postulaciones_por_comuna[nombre_comuna]
+        comuna['properties']['n_postulaciones_p'] = postulaciones_por_comuna[nombre_comuna]/postulaciones_totales
+    
+    if nombre_comuna in vacantes_por_comuna:
+        comuna['properties']['vacantes'] = vacantes_por_comuna[nombre_comuna]
+        comuna['properties']['vacantes_p'] = vacantes_por_comuna[nombre_comuna]/vacantes_totales
+print("Información agregada")
+
+
+print("Generando archivo geojson modificado...")
+with open('../data/comunas_ps.geojson', 'w') as f:
+  json.dump(datos, f, indent=4)
+  print('Archivo generado con éxito!')
